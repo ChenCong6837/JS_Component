ef649cc507fad26945fccf666a105377e814b718
chencong <337812462@qq.com>
2017-10-09 13:57:58 +0800

    寻找一段文字中最长的单词

--- js_method/tips.js
+++ js_method/tips.js
@@ -3,7 +3,7 @@ function reverseStr(str){
      return str.split('').reverse().join('');
 }
 
-//2.回文串判断，去除其他符号
+//2.回文串判断，去除其他符号，是回文串就返回true，否则返回false
 function palindrome(str) {
   var strToArr = str.toLowerCase().split('').filter(function(val){
      return (val >= 'a' && val <= 'z')||(val >= '0' && val <= '9');
@@ -11,3 +11,15 @@ function palindrome(str) {
    var temp = strToArr.join('');
    return temp === strToArr.reverse().join('');
  }
+
+ //3.寻找一段文字中最长的单词
+ function findLongestWord(str) {
+     var strToArr = str.split(' ');
+     var longestWord = strToArr[0];
+     for(var i = 0, len = strToArr.length; i < len; i++){
+       if(longestWord.length < strToArr[i].length){
+         longestWord = strToArr[i];
+       }
+     }
+     return longestWord;
+   }
