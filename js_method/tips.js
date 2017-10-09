d6ac682eaac276188aa44f00b6a41633e49ebe1c
chencong <337812462@qq.com>
2017-10-09 13:45:32 +0800

    回文串判断

--- js_method/tips.js
+++ js_method/tips.js
@@ -2,3 +2,12 @@
 function reverseStr(str){
      return str.split('').reverse().join('');
 }
+
+//2.回文串判断，去除其他符号
+function palindrome(str) {
+  var strToArr = str.toLowerCase().split('').filter(function(val){
+     return (val >= 'a' && val <= 'z')||(val >= '0' && val <= '9');
+   });
+   var temp = strToArr.join('');
+   return temp === strToArr.reverse().join('');
+ }
