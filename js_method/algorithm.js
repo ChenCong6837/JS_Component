5e7324ec52d20e541b454e13b9e66e97c85e040b
chencong <337812462@qq.com>
2017-11-04 21:35:29 +0800

    去除整数数组中重复的值

--- js_method/algorithm.js
+++ js_method/algorithm.js
@@ -10,3 +10,24 @@ function checkPalindrom(str) {
 }
 
 checkPalindrom('aabbccddccbbaa'); //true
+
+//======================================================================================
+/**
+ * 问题：去除整数数组中重复的值
+ * @param arr：要去重的数组
+ * @return：去重后的数组
+ */
+
+function unique(arr) {
+     var hashTable = {};
+     var data = [];
+     for(var i = 0, len = arr.length; i < len; i++){
+          if(!hashTable[arr[i]]){
+               hashTable[arr[i]] = true;
+               data.push(arr[i]); 
+          }
+     }
+     return data;
+}
+
+unique([7,1,2,3,5,3,4,5,6,2,3,1]); //[7,1,2,3,5,4,6]
