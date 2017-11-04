12bc7ec9eaf0810194d8bf521337f830e24bbdd3
chencong <337812462@qq.com>
2017-11-04 21:36:53 +0800

    统计一个字符串出现最多的字母

--- js_method/algorithm.js
+++ js_method/algorithm.js
@@ -31,3 +31,36 @@ function unique(arr) {
 }
 
 unique([7,1,2,3,5,3,4,5,6,2,3,1]); //[7,1,2,3,5,4,6]
+
+//======================================================================================
+/**
+ * 问题：统计一个字符串出现最多的字母
+ * @param str：输入一个字符串
+ * @return ：出现最多次的字母
+ */
+function findMaxDuplicateChar(str) {
+     if(str.length === 1){
+          return str;
+     }
+     var charObj = {};
+     for(var i = 0, len = str.length; i < len; i++){
+          if(!charObj[str.charAt(i)]){
+               charObj[str.charAt(i)] = 1;
+          } else {
+               charObj[str.charAt(i)]++;
+          }
+     }
+     var maxChar = '',
+        maxValue = 1;
+        for(var k in charObj){
+             if(charObj[k] > maxValue){
+                  maxValue = charObj[k];
+                  maxChar = k;
+             }
+        }
+     return "出现最多次的字符：" + maxChar + "，出现了：" + maxValue + "次";
+}
+
+findMaxDuplicateChar('fasdfhjkasdhfiulwerasdfadaaaa');//出现最多次的字符：a，出现了：8次
+
+//======================================================================================
